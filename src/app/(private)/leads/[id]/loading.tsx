ffbb04b0f191dcb036ef4fb87bc069cc33c64cea
eronaralves <alveseronar@gmail.com>
2025-06-02 15:17:52 -0300

    fix: loading lead

--- src/app/(private)/leads/[id]/loading.tsx
+++ src/app/(private)/leads/[id]/loading.tsx
@@ -1,9 +1,7 @@
 export default function Loading() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <p className="mt-4 text-lg text-gray-600">
-        Carregando perfil do aluno...
-      </p>
+      <p className="mt-4 text-lg text-gray-600">Carregando perfil do lead...</p>
     </div>
   )
 }
