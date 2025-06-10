198f625672a8229f551a9d510272df0f9f3aba78
eronaravles <alveseronar@gmail.com>
2025-06-10 11:26:43 -0300

    fix: add journey tab

--- src/components/menu-nav-links.tsx
+++ src/components/menu-nav-links.tsx
@@ -5,7 +5,7 @@ import { usePathname } from 'next/navigation'
 
 // Icons
 import {
-  // Eye,
+  Eye,
   Mail,
   Users,
   UserCog,
@@ -34,11 +34,11 @@ export function ManuNavLinks() {
       href: '/cursos',
       icon: BookOpen,
     },
-    // {
-    //   label: 'Jornada',
-    //   href: '/jornada',
-    //   icon: Eye,
-    // },
+    {
+      label: 'Jornada',
+      href: '/jornada',
+      icon: Eye,
+    },
     {
       label: 'Equipe',
       href: '/equipe',
